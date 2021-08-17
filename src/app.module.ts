002ee08e2d728b4183aaa5c5452e0c4cd374575c
ruttvikkheni <khenirutvik2001@gmail.com>
2021-08-17 23:11:22 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { StudentModule } from './student/student.module';
+
+@Module({
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: true
+  }), LessonModule, StudentModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
